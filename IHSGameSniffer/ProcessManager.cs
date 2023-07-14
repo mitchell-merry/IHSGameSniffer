4903f35fad17205be4c6b411caf315d1b9c9cfbe
Mitchell Merry <mitchellmerry145@gmail.com>
2023-07-14 23:11:53 +1000

    initial commit

---
+++ IHSGameSniffer/ProcessManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHSGameSniffer
+{
+    public static class ProcessManager
+    {
+        private static Process _process;
+        public const string PROCESS_NAME = "Greylock-Win64-Shipping";
+
+        /// <summary>
+        /// Returns the process. Will cache results between runs.
+        /// </summary>
+        public static Process GetProcess(Action<Process> onFound, Action<Process> onExit)
+        {
+            try
+            {
+                // We have a game cached and it's good
+                if (_process != null && !_process.HasExited)
+                {
+                    return _process;
+                }
+
+                var proc = Process.GetProcesses().FirstOrDefault(x => PROCESS_NAME.ToLower().Contains(x.ProcessName.ToLower()));
+
+                if (proc != null && !proc.HasExited)
+                {
+                    // Cache the found process
+                    _process = proc;
+
+                    // Run event on finding the proc
+                    onFound?.Invoke(_process);
+
+                    return _process;
+                }
+
+                _process = null;
+            }
+            catch (Win32Exception e)
+            {
+                Console.WriteLine("Run the program as admin");
+            }
+            return null;
+        }
+    }
+}
--- IHSGameSniffer/Program.cs
+++ IHSGameSniffer/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveSplit.ComponentUtil;
 
 namespace IHSGameSniffer
 {
-    class Program
+    static class Program
     {
+        private const int WORLD = 0x56A6B60;
+        private static DeepPointer VelocityX = new DeepPointer(ProcessManager.PROCESS_NAME + ".exe", WORLD, 0x118, 0x3C8, 0x0, 0x260, 0x290, 0x140);
+        private static DeepPointer VelocityZ = new DeepPointer(ProcessManager.PROCESS_NAME + ".exe", WORLD, 0x118, 0x3C8, 0x0, 0x260, 0x290, 0x144);
+        private static DeepPointer VelocityY = new DeepPointer(ProcessManager.PROCESS_NAME + ".exe", WORLD, 0x118, 0x3C8, 0x0, 0x260, 0x290, 0x148);
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Process proc = ProcessManager.GetProcess(null);
+
+                if (proc == null)
+                {
+                    ClearAndWrite("Waiting for game to be launched...");
+                    System.Threading.Thread.Sleep(1000);
+                    continue;
+                }
+
+                ClearAndWrite("Game running!");
+
+                // Update on-screen values
+                try
+                {
+
+                    Console.SetCursorPosition(0, 2);
+
+                    var velX = VelocityX.Deref<float>(proc);
+                    var velZ = VelocityZ.Deref<float>(proc);
+                    var velY = VelocityY.Deref<float>(proc);
+
+                    var velHori = Math.Sqrt(Math.Pow(velX, 2) + Math.Pow(velZ, 2));
+                    ClearAndWrite("Horizontal Velocity: " + velHori);
+                    ClearAndWrite("Veritcal Velocity: " + velY);
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                }
+
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        static void ClearAndWrite(string line)
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(line + new string(' ', Console.WindowWidth - line.Length));
         }
     }
 }
